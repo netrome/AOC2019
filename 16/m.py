3ca6b49b26632acc63d2888bfef1874e7878a42a
Mårten Nilsson <m@martennilsson.se>
2019-12-27 22:53:37 +0100

    Day 16 part 2 solved, but there is room for efficiency improvements.
    The backward fft algorithm can be rewritten to accept the number of
    phases to simulate. Instead of just summing, it can multiply each number
    in the backward series by the number of phases minus the offset to the
    end of the series (or something similar to this). It should be possible
    to find a constellation like this that yields the same results but much
    faster.

--- 16/m.py
+++ 16/m.py
@@ -1,4 +1,5 @@
 import itertools as it
+import math
 import functools
 
 
@@ -10,18 +11,40 @@ def pattern(n):
     return pattern_iterator
 
 
+def pattern_at(n, i):
+    shifted = i + 1
+    n_plus_one = n + 1
+    return round(math.sin( (shifted // n_plus_one % 4) * math.pi / 2))
+
+
 def nth_fft_element(numbers, n):
     p = pattern(n)
-    s = sum(map(lambda t: t[0]*t[1], zip(numbers, p)))
+    s = sum(map(lambda t: t[0] * t[1], zip(numbers, p)))
+    return abs(s) % 10
+
+
+def nth_fft_element_alt(numbers, n):
+    s = sum(map(lambda t: t[1]*pattern_at(n, t[0]), enumerate(numbers)))
     return abs(s) % 10
 
 
 def fft_phase(numbers):
-    return tuple(map(functools.partial(nth_fft_element, numbers), range(len(numbers))))
+    return tuple(map(functools.partial(nth_fft_element_alt, numbers), range(len(numbers))))
 
 
-def part1(digit):
-    vals = tuple(map(int, str(digit)))
+def backward_fft(numbers):
+    """ Much faster than the forward one, yields the correct values for the latter half of the fft """
+    def accumulate():
+        s = 0
+        for val in reversed(numbers):
+            s += val
+            yield s
+
+    return tuple(reversed(tuple(map(lambda x: abs(x) % 10, accumulate()))))
+
+
+def part1(digits):
+    vals = tuple(map(int, str(digits)))
 
     for _ in range(100):
         vals = fft_phase(vals)
@@ -29,14 +52,22 @@ def part1(digit):
     return "".join(map(str, vals[:8]))
 
 
-def part2(digit):
-    vals = tuple(it.repeat(map(int, str(digit)), 1000))
-    print(vals)
+def part2(digits):
+    vals = tuple(list(map(int, str(digits))) * 10_000)
+    offset = int(str(digits)[:7])
+    print(offset)
+
+    for _ in range(100):
+        vals = backward_fft(vals)
+
+    return "".join(map(str, vals[offset:offset + 8]))
 
 my_input = int(open("in").read().strip())
 test0 = 12345678
 test1 = 80871224585914546619083218645595
 test2 = 19617804207202209144916044189917 
-print(f"Part1 {part1(my_input)}")
 
+print(f"Part1 {part1(test1)}")
 
+test3 = "03036732577212944063491565474664"
+print(f"Part2 {part2(my_input)}")
