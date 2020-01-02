5a9b970a7ba653ebbfb89c0d018735aa2472ffd8
Mårten Nilsson <m@martennilsson.se>
2020-01-02 21:23:08 +0100

    More progress on day 20

--- 20/m.py
+++ 20/m.py
@@ -1,11 +1,17 @@
 from collections import defaultdict
 from dataclasses import dataclass
 
+import string
+
+
+DIRECTIONS = (-1, 1, -1j, 1j)
+
 
 @dataclass
 class Maze:
     grid: dict
     portals: dict
+    reverse_portals: dict
     start: complex
     finish: complex
 
@@ -20,19 +26,37 @@ def _get_grid(lines):
 
 def portal(point, grid):
     """ Returns the name of the portal if the point is a portal, else None """
-    pass
+    for direction in DIRECTIONS:
+        if grid[point + direction] in string.ascii_uppercase:
+            letters = [grid[point + direction], grid[point + direction * 2]]
+            if direction in [-1, -1j]:
+                return "".join(reversed(letters))
+            return "".join(letters)
+
+    return None
 
 
 def get_maze(lines):
     grid = _get_grid(lines)
+    portals = defaultdict(set)
+    reverse_portals = {}
 
     open_spaces = map(lambda t: t[0], filter(lambda t: t[1] == ".", grid.items()))
     for point in open_spaces:
         maybe_portal = portal(point, grid)
+        if maybe_portal:
+            portals[maybe_portal].add(point)
+            reverse_portals[point] = maybe_portal
+
+    start = portals["AA"]
+    finish = portals["ZZ"]
+
+    return Maze(grid, dict(portals), reverse_portals, start, finish)
 
 
 def part1():
     l = open("in").readlines()
+    maze = get_maze(l)
 
 
 part1()
